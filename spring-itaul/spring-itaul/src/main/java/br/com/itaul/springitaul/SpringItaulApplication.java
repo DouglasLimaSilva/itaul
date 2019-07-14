dd4727f57c6634953dafdb9985f696e2e2338adb
DouglasLimaSilva <0douglaslima0@gmail.com>
2019-07-14 20:19:02 -0300

    plano itau com spring mvc

---
+++ spring-itaul/spring-itaul/src/main/java/br/com/itaul/springitaul/SpringItaulApplication.java
@@ -0,0 +1,13 @@
+package br.com.itaul.springitaul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringItaulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringItaulApplication.class, args);
+	}
+
+}
---
+++ spring-itaul/spring-itaul/src/test/java/br/com/itaul/springitaul/SpringItaulApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.itaul.springitaul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringItaulApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
